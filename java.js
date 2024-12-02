89576d26adb57430f048e0af30c9b98e007dc23e
kilerzi <158763368+kilerzi@users.noreply.github.com>
2024-12-02 23:53:19 +0100

    Delete java.js

--- java.js
+++
@@ -1,9 +0,0 @@
-console.log ("today you are buying 30 phones,one phone is 119.95 plus the addition 5% tax");
-let price = 119.95;
-let tax = price / 20;
-let priceofphone = price + tax;
-console.log(typeof priceofphone);
-console.log(priceofphone);
-let priceofeverything = priceofphone * 30;
-console.log(typeof priceofeverything)
-console.log(priceofeverything)
